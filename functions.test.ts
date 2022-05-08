8243b675727d02793b84fb7ff08a53b0631cd67f
Maria Anderson <Mandersonreka@gmail.com>
2022-05-07 18:02:12 -0600

    make it better

--- functions.test.ts
+++ functions.test.ts
@@ -1,23 +1,25 @@
 const {shuffleArray} = require('./utils')
 
+const arr = [1, 2, 3, 4]
+
 describe('shuffleArray should', () => {
-    test("shows a return of an array", () => {
-        let testArray = [1, 2, 3, 4, 5];
-        let shuffleTest = shuffleArray(testArray);
-        let showType = Array.isArray(shuffleTest);
-        expect(showType).toBeTruthy();
-    }),
-    
-    test("shows an array of the same length", () =>{
-        let testArray = [1,2,3,4,5];
-        let shuffleTest = shuffleArray(testArray);
-        let sameLength = true;
-        if (testArray.length === shuffleTest.length){
-            sameLength = true;
+
+    let res = shuffleArray(arr)
+    test('return an array', () => {
+        expect(Array.isArray(res)).toBeTruthy()
+    })
+
+    test('return an array of the same length as the argument passed to it', () => {
+        expect(res.length).toEqual(arr.length)
+    })
+
+    test('return an array that contains the same items as the argument passed to it', () => {
+        for(let i = 0; i < res.length; i ++){
+            expect(arr.includes(res[i])).toBeTruthy()
         }
-        else{
-            sameLength = false;
-        }
-        expect(sameLength).toBeTruthy();
+    })
+
+    test('return an array with items in a different order than the argument', () => {
+        expect(res).not.toEqual(arr)
     })
 })
