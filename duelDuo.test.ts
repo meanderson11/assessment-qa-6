f4c1a5a6d57846f4b9af46be705631823957bd64
Maria Anderson <Mandersonreka@gmail.com>
2022-05-10 21:33:14 -0600

    initial commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -10,34 +10,38 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-    driver.sleep(1000)
-    driver.quit()
+    await driver.sleep(1000)
 })
 
-// test('Title shows up when page loads', async () => {
-//     const title = await driver.findElement(By.id('title'))
-//     const displayed = await title.isDisplayed()
-//     expect(displayed).toBe(true)
-// })
+afterAll(async () => {
+    await driver.quit()
+})
 
-driver.sleep(1000)
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+
 
 test('Clicking draw button displays bot choices', async () => {
     const draw = await driver.findElement(By.id('draw'))
     await draw.click()
+    await driver.sleep(2000)
     const choices = await driver.findElement(By.id('choices'))
     const displayed = await choices.isDisplayed()
     expect(displayed).toBe(true)
 })
 
-// test('Clicking "Add to Duo" displays "Your Duo"', async () => {
-//     await driver.findElement(By.id('draw')).click()
-//     driver.sleep(300)
-//     await driver.findElement(By.xpath('//button[@class="bot-btn"]')).click()
-//     driver.sleep(300)
-//     const duo = await driver.findElement(By.id('player-duo'))
-//     const displayed = await duo.isDisplayed()
-//     expect(displayed).toBeTruthy()
-// })
+test('Clicking "Add to Duo" displays "Your Duo"', async () => {
+    await driver.findElement(By.id('draw')).click()
+    driver.sleep(300)
+    await driver.findElement(By.xpath('//button[@class="bot-btn"]')).click()
+    driver.sleep(300)
+    const duo = await driver.findElement(By.id('player-duo'))
+    const displayed = await duo.isDisplayed()
+    expect(displayed).toBeTruthy()
+})
 
-
+// driver.quit()
